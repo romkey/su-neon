5d50993e73fb15e626b5e2699a3c43b850640508
John Romkey <romkey@romkey.com>
2017-10-09 12:57:48 -0700

    fix database migrations...

--- db/migrate/20170927004920_create_signs.rb
+++ db/migrate/20170927004920_create_signs.rb
@@ -2,7 +2,7 @@ class CreateSigns < ActiveRecord::Migration[5.1]
   def change
     create_table :signs do |t|
       t.string :name, null: false
-      t.belongs_to :particle, foreign_key: true
+      t.belongs_to :particle_instances, foreign_key: true
       t.integer :relay, null: false
 
       t.timestamps
