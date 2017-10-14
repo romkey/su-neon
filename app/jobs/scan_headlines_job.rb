4f03a5c67d1375d52e7ee6edc281c96b6512d02d
John Romkey <romkey@romkey.com>
2017-10-14 15:42:21 -0700

    try not to fail

--- app/jobs/scan_headlines_job.rb
+++ app/jobs/scan_headlines_job.rb
@@ -6,24 +6,28 @@ class ScanHeadlinesJob < ApplicationJob
   queue_as :default
 
   def perform(source)
-    feed_file = open source.feed_url
+    begin
+      feed_file = open source.feed_url
 
-    parser = RSS::Parser.new feed_file
-    feed = parser.parse
-    feed.items.each do |item|
-      unless RecentHeadline.find_by link: item.link
-        RecentHeadline.create headline: item.title,
-                              link: item.link,
-                              news_source: source
-      end
+      parser = RSS::Parser.new feed_file
+      feed = parser.parse
+      feed.items.each do |item|
+        unless RecentHeadline.find_by link: item.link
+          RecentHeadline.create headline: item.title,
+                                link: item.link,
+                                news_source: source
+        end
 
-      Keyword.find_each do |keyword|
-        if item.title.downcase.match keyword.name
-          keyword.sign.turn_on
+        Keyword.find_each do |keyword|
+          if item.title.downcase.match keyword.name
+            keyword.sign.turn_on
+          end
         end
       end
-    end
 
-    source.update_attributes(last_processed_at: Time.now)
+      source.update_attributes(last_processed_at: Time.now)
+    rescue
+      return
+    end
   end
 end
