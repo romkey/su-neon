062d0333b0742f953317b7bc1ca330b9216dbea9
John Romkey <romkey@romkey.com>
2017-10-29 10:14:29 -0700

    attempt not to try to tickle Particle if there's no access token

--- app/models/particle_instance.rb
+++ app/models/particle_instance.rb
@@ -1,10 +1,11 @@
 class ParticleInstance < ApplicationRecord
   has_many :sign
 
-  
   # https://docs.particle.io/datasheets/particle-shields/#relay-shield-sample-code
   # curl https://api.particle.io/v1/devices/0123456789abcdef/relay -d access_token=123412341234 -d params=r1,HIGH
   def set_relay(relay, state)
+    return unless Config.first.particle_access_token.present?
+
     uri = URI.parse "https://api.particle.io/v1/devices/#{particle_id}/relay"
 
     header = { Authorization: "Bearer #{Config.first.particle_access_token }" }
