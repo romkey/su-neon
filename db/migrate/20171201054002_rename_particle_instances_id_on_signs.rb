5e79604a1d9372a46433b521b85607202205f237
John Romkey <romkey@romkey.com>
2017-11-30 21:42:46 -0800

    fix migration

--- db/migrate/20171201054002_rename_particle_instances_id_on_signs.rb
+++ db/migrate/20171201054002_rename_particle_instances_id_on_signs.rb
@@ -1,5 +1,5 @@
 class RenameParticleInstancesIdOnSigns < ActiveRecord::Migration[5.1]
   def change
-    rename :signs, :particle_instances_id, :particle_instance_id
+    rename_column :signs, :particle_instances_id, :particle_instance_id
   end
 end
