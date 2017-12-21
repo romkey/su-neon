f6f753245bd14017c5866383f618302923dc6bb7
John Romkey <romkey@romkey.com>
2017-12-20 22:19:29 -0800

    fix set_relay

--- app/models/sign.rb
+++ app/models/sign.rb
@@ -7,22 +7,23 @@ class Sign < ApplicationRecord
   validates :relay, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
 
   def turn_on
-    set_relay
+    set_relay true
     update_attributes(lit: true)
   end
 
   def turn_off
-    set_relay
+    set_relay false
     update_attributes(lit: false)
   end
 
   private
-  def set_relay
-#    Particle.access_token = Config.first.particle_access_token
 
-#    device = Particle.device(particle_instance.particle_id)
-    # we want to set r1, r2, r3 or r4 to LOW for off and HIGH for on
-    # https://docs.particle.io/datasheets/particle-shields/#relay-shield-sample-code
-#    device.function('digitalWrite', )
+  def set_relay(state)
+    return unless particle_instance_id
+
+    p = ParticleInstance.find particle_instance_id
+    return unless p
+
+    p.set_relay relay, state 
   end
 end
